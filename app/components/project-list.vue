08c284170f8bafa8fadd267ca0faec406e68997f
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-12-24 23:01:15 +0300

    fetch data

---
+++ app/components/project-list.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h2>Projects</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+const githubURL = "https://api.github.com/users/Alzubair98/repos";
+const { data } = await useFetch(githubURL);
+
+console.log(data.value);
+</script>
