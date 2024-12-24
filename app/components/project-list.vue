0766ddbdd091aeb590b847b993d2d1104ac1fd03
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-12-24 23:28:28 +0300

    fetch data using usefetch

--- app/components/project-list.vue
+++ app/components/project-list.vue
@@ -2,11 +2,14 @@
   <div>
     <h2>Projects</h2>
   </div>
+
+  <section v-if="pending">Loading...</section>
+  <section v-else-if="error">Error... Try again</section>
+  <section v-else>Repos!!</section>
 </template>
 
 <script setup lang="ts">
 const githubURL = "https://api.github.com/users/Alzubair98/repos";
-const { data } = await useFetch(githubURL);
 
-console.log(data.value);
+const { error, pending, data } = await useFetch(githubURL);
 </script>
